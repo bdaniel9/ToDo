1d96912bbbfe9d779a430b8527432f2dcd0617f9
bdaniel9 <bochisdaniel3@gmail.com>
2025-07-19 23:22:17 +0200

    Switch case in program

---
+++ src/main/java/hu/bd/AddToDo.java
@@ -0,0 +1,4 @@
+package hu.bd;
+
+public class AddToDo {
+}
--- src/main/java/hu/bd/Main.java
+++ src/main/java/hu/bd/Main.java
@@ -1,7 +1,35 @@
 package hu.bd;
 
+import java.util.Scanner;
+
 public class Main {
+    public void menu(){
+        Scanner scanner = new Scanner(System.in);
+        int option;
+
+        System.out.println("Choose an option:");
+        System.out.println("1. Add ToDo");
+        System.out.println("2. ToDo list");
+        System.out.println("0. Exit");
+
+        option = scanner.nextInt();
+
+
+        switch (option) {
+            case 1:
+                System.out.println("Add a ToDo to your list.");
+                break;
+            case 2:
+                System.out.println("View ToDo list.");
+                break;
+            case 3:
+                System.out.println("Exit.");
+                break;
+            default:
+                System.out.println("Invalid option!");
+                break;
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
     }
 }
