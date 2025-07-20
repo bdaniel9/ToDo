bd4297c270235d63e04f0a5029f7702c7826f87d
bdaniel9 <bochisdaniel3@gmail.com>
2025-07-20 11:19:37 +0200

    Do-While in Main

--- src/main/java/hu/bd/Main.java
+++ src/main/java/hu/bd/Main.java
@@ -1,33 +1,53 @@
 package hu.bd;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int option;
+        private final List<ToDo> toDoList = new ArrayList<>();
+        private final ViewToDo viewToDo = new ViewToDo();
 
-        System.out.println("Choose an option:");
-        System.out.println("1. Add ToDo");
-        System.out.println("2. ToDo list");
-        System.out.println("0. Exit");
+        public void menu() {
+            Scanner scanner = new Scanner(System.in);
+            int option;
 
-        System.out.print("Option: ");
-        option = scanner.nextInt();
+            do{
 
-        switch (option) {
-            case 0:
-                System.out.println("Goodbye.");
-                break;
-            case 1:
-                System.out.println("Add a ToDo to your list. ");
-                break;
-            case 2:
-                System.out.println("View ToDo list.");
-                break;
-            default:
-                System.out.println("Invalid option!");
-                break;
+            System.out.println("Choose an option:");
+            System.out.println("1. Add ToDo");
+            System.out.println("2. ToDo list");
+            System.out.println("0. Exit");
+
+            System.out.print("Option: ");
+            option = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (option) {
+                case 0:
+                    System.out.println("Goodbye.");
+                    break;
+                case 1:
+                    System.out.println("Enter title: ");
+                    String title = scanner.nextLine();
+                    System.out.println("Enter description: ");
+                    String description = scanner.nextLine();
+                    toDoList.add(new ToDo(title,description));
+                    System.out.println("Todo added!");
+                    break;
+                case 2:
+                    System.out.println("Show ToDo list.");
+                    viewToDo.display(toDoList);
+                    break;
+                default:
+                    System.out.println("Invalid option!");
+                    break;
+            }
+            }while (option!=0);
+            scanner.close();
         }
+
+    public static void main(String[] args) {
+        new Main().menu();
     }
 }
--- src/main/java/hu/bd/ViewToDo.java
+++ src/main/java/hu/bd/ViewToDo.java
@@ -1,4 +1,16 @@
 package hu.bd;
 
+import java.util.List;
+
 public class ViewToDo {
+    public void display(List<ToDo> toDoList){
+        if (toDoList.isEmpty()){
+            System.out.println("No ToDo found!");
+        }else {
+            System.out.println("Your ToDos: ");
+            for (ToDo toDo : toDoList){
+                System.out.println(toDo);
+            }
+        }
+    }
 }
